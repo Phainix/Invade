3cfb01cf15a7ca05a63257b0091d1660bc9fdb44
Phainix <28295747+Phainix@users.noreply.github.com>
2019-04-13 09:46:48 +0100

    Products page

---
+++ app/components.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    var components = [
+        {
+            'link': 'header.html',
+            'element': '#header'
+        },
+        {
+            'link': 'footer.html',
+            'element': '#footer'
+        },
+        {
+            'element': 'body',
+            'link': 'modal.html'
+        }
+    ];
+    for (var i = 0; i < components.length; i++) {
+        (function(index) {
+            $.get("components/"+components[i].link, function(i) {
+
+            }).done(function(data) {
+                $(components[index].element).append(data);
+
+                if(components[index].link == 'modal.html') {
+                    $(".iziModal").iziModal();
+                }
+            });
+
+        })(i);
+    }
+});
